b59be7e80c9f2bef873300a27bf88b0901b3bc83
doshi-jay <jay007199504@gmail.com>
2019-05-29 15:12:00 -0700

    Updating interaction window

--- interaction_window.py
+++ interaction_window.py
@@ -3,12 +3,13 @@ from tkinter import scrolledtext
 from tkinter import *
 
 root = tk.Tk()
+root.title("Smart Assistant!")
 
 button_frame = tk.Frame(root)
 button_frame.pack(fill=tk.X, side=tk.TOP)
 
 hello_label = Label(button_frame, text="Welcome User!")
-hello_label.configure(font=("Cambria", 12))
+hello_label.configure(font=("Cambria", 11))
 help_button = tk.Button(button_frame, text='Help', bg="black", fg="white")
 
 button_frame.columnconfigure(0, weight=1)
@@ -17,10 +18,10 @@ button_frame.columnconfigure(1, weight=1)
 hello_label.grid(row=0, column=0, sticky=tk.W, padx=10)
 help_button.grid(row=0, column=1, sticky=tk.E+tk.W, padx=10, pady=10)
 
-txt = scrolledtext.ScrolledText(root, width=60,height=10)
-txt.insert(tk.INSERT,'Please enter the command!')
-txt.pack()
+txt = scrolledtext.ScrolledText(root,height=10)
+txt.insert(tk.INSERT,'Please enter the command!\n')
 txt.focus()
+txt.pack()
 
 submit_frame = tk.Frame(root)
 submit_frame.pack(fill=tk.X)
@@ -30,8 +31,9 @@ submit_frame.columnconfigure(0, weight=1)
 submit_button = tk.Button(submit_frame, text='Submit', bg="black", fg="white")
 submit_button.grid(row=0, column=0, sticky=tk.W+tk.E, padx = 60, pady=10)
 
-output = scrolledtext.ScrolledText(root, width=60,height=10)
+output = scrolledtext.ScrolledText(root, height=15)
 output.insert(tk.INSERT,'Output:\n')
+output.configure(state=tk.DISABLED, bg="#d9dbdd")
 output.pack()
 
 root.mainloop()
