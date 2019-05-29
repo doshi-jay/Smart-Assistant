7dea752f9b28d97f6874a959d7e92157f26b6adc
mitishagd <dodderi.m@husky.neu.edu>
2019-05-28 23:35:24 -0700

    added output box, calculator functions'

---
+++ calculator.py
@@ -0,0 +1,58 @@
+import math
+
+class calculator:
+	
+	# Based on the operator, calls the corresponding 
+	# operation
+	def calculate(self, operand1, operand2, operator):
+		if operator == '+':
+			return self.add(operand1, operand2)
+		elif operator == '-':
+			return self.subtract(operand1, operand2)
+		elif operator == '*':
+			return self.multiply(operand1, operand2)
+		elif operator == '/':
+			return self.divide(operand1, operand2)
+		elif operator == '%':
+			return self.modulus(operand1, operand2)
+		elif operator == '**':
+			return self.power(operand1, operand2)
+
+	def add(self, operand1, operand2):
+		return operand1 + operand2
+
+	def subtract(self, operand1, operand2):
+		return operand1 - operand2
+
+	def multiply(self, operand1, operand2):
+		return operand1 * operand2
+
+	def divide(self, operand1, operand2):
+		if operand2 == 0:
+			print "Divide by zero error"
+			return
+		return operand1 / operand2
+
+	def modulus(self, operand1, operand2):
+		return operand1 % operand2
+
+	def power(self, base, power):
+		return base**power
+
+	def sine(self, operand):
+		return math.sin(operand)
+
+	def cosine(self, operand):
+		return math.cos(operand)
+
+	def tan(self, operand):
+		return math.tan(operand)
+
+	def secant(self, operand):
+		return (1/self.cosine(operand))
+
+	def cosecant(self, operand):
+		return (1/self.sine(operand))
+
+	def cotan(self, operand):
+		return (1/self.tan(operand))
--- interaction_window.py
+++ interaction_window.py
@@ -23,11 +23,15 @@ txt.pack()
 txt.focus()
 
 submit_frame = tk.Frame(root)
-submit_frame.pack(fill=tk.X, side=tk.BOTTOM)
+submit_frame.pack(fill=tk.X)
 
 submit_frame.columnconfigure(0, weight=1)
 
 submit_button = tk.Button(submit_frame, text='Submit', bg="black", fg="white")
 submit_button.grid(row=0, column=0, sticky=tk.W+tk.E, padx = 60, pady=10)
+
+output = scrolledtext.ScrolledText(root, width=60,height=10)
+output.insert(tk.INSERT,'Output:\n')
+output.pack()
 
 root.mainloop()
